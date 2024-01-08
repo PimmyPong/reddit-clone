4fb917beb47a2b6fef51c9a15d971ed6d48966cc
pimpa pongsopa <you@example.compp-psp@hotmail>
2024-01-07 22:36:19 -0800

    adddynamic

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import { fetchUser } from "@/lib/fetchUser.js";
 import { prisma } from "@/lib/prisma.js";
 import SubReddits from "./subreddits/page.jsx";
 import Subreddit from "./subreddits/[subredditId]/page.jsx";
+export const dynamic = "force-dynamic";
 
 export default async function Home({ params }) {
 	const subreddits = await prisma.subreddit.findMany({
@@ -29,15 +30,6 @@ export default async function Home({ params }) {
 	return (
 		<div className="">
 			<SubReddits />
-			{/* <div className="home-posts">
-				{posts.map((post) => {
-					return (
-						<div key={post.id}>
-							<Subreddit post={post} params={params} />
-						</div>
-					);
-				})}
-			</div> */}
 		</div>
 	);
 }
